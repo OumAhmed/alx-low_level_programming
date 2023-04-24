bdbcfeca84d7032131f4477334fbf0c074c64496
oumaima <oumaima92alaoui@gmail.com>
2023-04-24 12:27:46 -0700

    My task6

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+* Description: 'Print numbers of base 10 using putchar'
+* Return: Always 0
+*/
+int main(void)
+{
+int n = 122;
+while (n >= 97)
+{
+putchar(n);
+n--;
+return (0);
+}
