577302351a590eb8ec897d54fbaaed413c0824ee
oumaima <oumaima92alaoui@gmail.com>
2023-04-26 07:37:02 -0700

    My task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - Entry point
-* Description: 'Prints_alphabet - Prints the alphabet in lowercase'
+* Description: print_alphabet - Prints the alphabet in lowercase'
 * Return: Always 0
 */
 void print_alphabet(void)
